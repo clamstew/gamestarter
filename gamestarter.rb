a8f697c27d6eeb96dfeed9fecabeba3b200dcfe0
bangner <joswagne@gmail.com>
2013-09-21 12:16:13 -0500

    put form elements as params

--- gamestarter.rb
+++ gamestarter.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+zrequire 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -22,4 +22,12 @@ post '/add_event' do
   @event_name = params[:event_name]
   # form in /new_event posts to here for processing
   "event sent. .. redirect to home #{@event_name}"
+  @creator_name = params[:creator_name]
+  @creator_email = params[:creator_email]
+  @event_time = params[:event_time]
+  @location = params[:location]
+  @attendees = params[:attendees]
+  @minimum_attendees = params[:minimum_attendees]
+  @maximum_attendees = params[:maximum_attendees]
+  @deadline = params[:deadline]
 end
