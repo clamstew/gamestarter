a029eea92c812e4324afd461b2b323e6e360882d
Clay Stewart <claytonmstewart@gmail.com>
2013-09-21 00:57:57 -0500

    Set up front end framework of includes.  Style and frame out in html basic home, basic form for new event, and a route to accept the form post.

--- gamestarter.rb
+++ gamestarter.rb
@@ -4,5 +4,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  'hello world'
+
+  # 'hello world'
+  erb :index
+end
+
+get '/event' do
+  example_event = new GameStarter::Event.new(time, deadline, event_name, location, attendees, minimum, maximum, creator_name, phone, email)
+  example_event.inspect
+end
+
+get '/new_event' do
+  erb :add_event_form
+end
+
+post '/add_event' do
+  # form in /new_event posts to here for processing
+  'event sent. .. redirect to home'
 end
