855d7880bff66a560d1d6d24f01b03955c2b3be5
bangner <joswagne@gmail.com>
2013-09-21 12:24:44 -0500

    add form to firebase

--- gamestarter.rb
+++ gamestarter.rb
@@ -24,10 +24,15 @@ post '/add_event' do
   "event sent. .. redirect to home #{@event_name}"
   @creator_name = params[:creator_name]
   @creator_email = params[:creator_email]
+  @creator_phone = params[:creator_phone]
   @event_time = params[:event_time]
   @location = params[:location]
   @attendees = params[:attendees]
   @minimum_attendees = params[:minimum_attendees]
   @maximum_attendees = params[:maximum_attendees]
   @deadline = params[:deadline]
+
+new_event = new GameStarter::Event.new(@time, @deadline, @event_name, @location, @minimum_attendees, @maximum_attendees, @creator_name, @creator_phone, @creator_email)
+new_event.add_to_firebase
 end
+
