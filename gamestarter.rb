67d6c914647371777a8ce4107b93d2c7aa76e1c4
bangner <joswagne@gmail.com>
2013-09-21 12:46:37 -0500

    fixed spelling issues and renamed params

--- gamestarter.rb
+++ gamestarter.rb
@@ -1,7 +1,10 @@
-zrequire 'rubygems'
+require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
+
+require_relative 'lib/event'
 
 get '/' do
 
@@ -10,7 +13,7 @@ get '/' do
 end
 
 get '/event' do
-  example_event = new GameStarter::Event.new(time, deadline, event_name, location, attendees, minimum, maximum, creator_name, phone, email)
+  example_event = GameStarter::Event.new(time, deadline, event_name, location, attendees, minimum, maximum, creator_name, phone, email)
   example_event.inspect
 end
 
@@ -21,18 +24,20 @@ end
 post '/add_event' do
   @event_name = params[:event_name]
   # form in /new_event posts to here for processing
-  "event sent. .. redirect to home #{@event_name}"
   @creator_name = params[:creator_name]
   @creator_email = params[:creator_email]
   @creator_phone = params[:creator_phone]
   @event_time = params[:event_time]
-  @location = params[:location]
+  @event_location = params[:event_location]
   @attendees = params[:attendees]
   @minimum_attendees = params[:minimum_attendees]
   @maximum_attendees = params[:maximum_attendees]
   @deadline = params[:deadline]
 
-new_event = new GameStarter::Event.new(@time, @deadline, @event_name, @location, @minimum_attendees, @maximum_attendees, @creator_name, @creator_phone, @creator_email)
-new_event.add_to_firebase
+  new_event = GameStarter::Event.new(@event_time, @deadline, @event_name, @event_location, @minimum_attendees, @maximum_attendees, @creator_name, @creator_phone, @creator_email) 
+  
+  binding.pry
+
+  new_event.add_to_firebase
 end
 
