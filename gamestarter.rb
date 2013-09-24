9e12e1e12308863608410e7b534a668d17ad4108
Clay Stewart <claytonmstewart@gmail.com>
2013-09-24 17:02:38 -0500

    Add all the methods for email sending with Mandrill API, and pushing to production

--- gamestarter.rb
+++ gamestarter.rb
@@ -49,11 +49,13 @@ post '/add_event' do
 
   @event_id = result.raw_body[9..-3]  
 
-  # Send an email
-  # email = GameStarter::Email.new
-  # email.send(new_event.invitees, @event_id.to_s)
+  # Send an email to invitees
   email = GameStarter::MandrillEmail.new()
-  email.send_are_you_in
+  email.send_are_you_in(new_event.invitees, @event_id.to_s)
+
+  # Send email to event creator
+  email2 = GameStarter::MandrillEmail.new()
+  email2.send_event_create_confirmation(@creator_email, @event_id.to_s, @event_name)
 
   erb :form_result
 end
@@ -71,52 +73,48 @@ end
 
 # Route for when attendees click "I'm In"
 post '/im_in' do
-  # def json_to_sym_hash(json)
-  #   json.gsub!('\'', '"')
-  #   parsed = JSON.parse(json)
-  #   symbolize_keys(parsed)
-  # end
-
-  # def symbolize_keys(hash)
-  #   hash.inject({}){|new_hash, key_value|
-  #     key, value = key_value
-  #     value = symbolize_keys(value) if value.is_a?(Hash)
-  #     new_hash[key.to_sym] = value
-  #     new_hash
-  #   }
-  # end
-
 
   @event_id = params[:event_id]
   @attendee_email = params[:invitee_email]
   @attendees = Unirest::get("https://gamestarter.firebaseio.com/events/#{@event_id}/attendees/.json",
   { "Accept" => "application/json" })
 
+  # Get the minimum number of attendees for this event
   @minimum_attendees = Unirest::get("https://gamestarter.firebaseio.com/events/#{@event_id}/minimum/.json",
   { "Accept" => "application/json" })
   @minimum_attendees = @minimum_attendees.body
   @minimum_attendees = @minimum_attendees.gsub(/[^0-9]/,'')
   @minimum_attendees = @minimum_attendees.to_i
 
-  @maximum_attendees = Unirest::get("https://gamestarter.firebaseio.com/events/#{@event_id}/minimum/.json",
+  # Get the maximum number of attendees for this event
+  @maximum_attendees = Unirest::get("https://gamestarter.firebaseio.com/events/#{@event_id}/maximum/.json",
   { "Accept" => "application/json" })
   @maximum_attendees = @maximum_attendees.body
   @maximum_attendees = @maximum_attendees.gsub(/[^0-9]/,'')
   @maximum_attendees = @maximum_attendees.to_i
+
+  # Get the maximum number of attendees for this event
+  @event_name = Unirest::get("https://gamestarter.firebaseio.com/events/#{@event_id}/event_name/.json",
+  { "Accept" => "application/json" })
+  @event_name = @event_name.body
+
+  # if attendees in this firebase event is empty
   if @attendees.raw_body == "null"
     array = {}
     @attendees_body = @attendees.body
     @attendees_body = array
-    # @attendees_body = array << @attendee_email
-      new_attendees_array = @attendees_body[:email1] = @attendee_email
-    ### @attendees_json = @attendees.body.to_a << 
-    # @attendees_json = @attendees_body.to_json
+    new_attendees_array = @attendees_body[:email1] = @attendee_email
     response = Unirest::post("https://gamestarter.firebaseio.com/events/#{@event_id}/attendees/.json",
     { "Accept" => "application/json" }, new_attendees_array.to_json)
+    @counter_of_attendees = 1
+
+  # if attendees in this firebase event has 1 or more emails in it
   elsif @attendees.raw_body != "null"
-    # @attendees_id = @attendees.body[0] # 2..21
       @attendees_id = @attendees.body
       
+      # if attendees in this firebase event has ONLY 1 email in it
+      # it will appear as a hash and need to be treated as one 
+      # - and turned into an array which is what we want
       if @attendees_id.is_a? Hash
         i = 0
         @attendees_id.each do |x,y| 
@@ -127,8 +125,6 @@ post '/im_in' do
         end
         @counter_of_attendees = i
 
-
-
         @new_attendees_array = @attendees_id[@first_key] = @attendees_id[@first_key] + ", #{@attendee_email}" 
         @new_attendees_array = @new_attendees_array.split(',')
         @new_attendees_array.collect! do |x|
@@ -137,14 +133,9 @@ post '/im_in' do
         response = Unirest::put("https://gamestarter.firebaseio.com/events/#{@event_id}/attendees/.json",
         { "Accept" => "application/json" }, @new_attendees_array.to_json)
 
-        # if @counter_of_attendees >= @maximum_attendees
-          # send email to all attendees 
-          # email = GameStarter::Email.new
-          # email.send_game_on(@new_attendees_array, @event_id)
-        # end
-
+      # if attendees in this firebase event has GREATER THAN 1 email in it  
+      # it will appear as an array and you can do array pushes on it
       elsif @attendees_id.is_a? Array
-        # @new_attendees_array = 'this came back as an array'
         @new_attendees_array = @attendees_id << @attendee_email
         @counter_of_attendees = @new_attendees_array.count
         response = Unirest::put("https://gamestarter.firebaseio.com/events/#{@event_id}/attendees/.json",
@@ -153,30 +144,22 @@ post '/im_in' do
         if @counter_of_attendees == @minimum_attendees
           # send email to all attendees 
           # Send an email
-          email_game_on = GameStarter::Email.new
-          email_game_on.send_game_on(@new_attendees_array, @event_id)
+          # email_game_on = GameStarter::Email.new
+          # email_game_on.send_game_on(@new_attendees_array, @event_id)
+          email_game_on = GameStarter::MandrillEmail.new
+          email_game_on.send_game_on(@new_attendees_array, @event_id, @event_name)
+
         elsif @counter_of_attendees > @minimum_attendees && @counter_of_attendees <= @maximum_attendees
-          
-          email_game_on = GameStarter::Email.new
-          email_game_on.send_game_already_on(@attendee_email, @event_id)
+
+          email_game_on = GameStarter::MandrillEmail.new
+          email_game_on.send_game_already_on(@attendee_email, @event_id, @event_name)
         
         else @counter_of_attendees > @maximum_attendees
-          email_game_full = GameStarter::Email.new
-          email_game_full.send_game_rejection(@attendee_email, @event_id)
+          email_game_full = GameStarter::MandrillEmail.new
+          email_game_full.send_game_rejection(@attendee_email, @event_id, @event_name)
+
         end
       end
   end
-  # @awesome = @response.body
-  # @response = JSON.load(@awesome)
-  # if @response['attendees'] == "null"
-  #   @response['attendees'] = []
-  #   @response['attendees'] << "#{@attendee_email}"
-  #   @response_json = response.to_json
-  #   @response = Unirest::post("https://gamestarter.firebaseio.com/events/#{@event_id}.json", { "Accept" => "application/json" }, @response_json)
-  # end
-
-  
-  # @attendees_count = @attendees.count
   erb :attend_confirmation
-
 end
--- lib/event.rb
+++ lib/event.rb
@@ -215,20 +215,151 @@ module GameStarter
   end
 
   class MandrillEmail
-    def send_are_you_in()
+    # Sends initial invite to potential attendees
+    #
+    #
+    def send_are_you_in(emails, event_id)
+      mandrill_to_array = []
+      mandrill_mergevars_array =[]
+      emails.each do |email|
+        email_url_encode = email.gsub("@", "%40")
+        this_to_object = {
+          :email => "#{email}",
+          :name => ""
+        }
+        mandrill_to_array.push(this_to_object)
+
+        this_merge_vars = {
+          :rcpt => "#{email}",
+          :vars => [
+            {
+              "name"=> "urlemail",
+              "content"=> "#{email}"
+            }, 
+            {
+              "name"=> "replyurl",
+              "content"=> "http://gamestarter.herokuapp.com/reply/#{event_id}/#{email_url_encode.strip()}"
+            }
+          ]
+        }
+        mandrill_mergevars_array.push(this_merge_vars)
+      end
+
       m = Mandrill::API.new
       message = {  
+       :merge_vars => mandrill_mergevars_array,
+       :merge => true,
        :subject=> "New Event on Gamestarter",  
        :from_name=> "GameStarter <noreply@eventstarter.co>",  
        :text=>"You have a new gamestarter event. Are you in?",  
-       :to=>[  
-         {  
-           :email=> "",  
-           :name=> "Joe Smith"  
-         }  
-       ],  
-       :html=>"<html><h1>Hi <strong>message</strong>, are you in?</h1><p>Yes, <a href="">I'm in!</a></p></html>",  
-       :from_email=>"sender@yourdomain.com"  
+       :to=> mandrill_to_array,  
+       :html=>"<html><h1>Hi <strong>message</strong>, are you in?</h1><p>Yes, <a href=" + '"*|REPLYURL|*"' + ">I'm in!</a></p></html>",  
+       :from_email=>"sender@gamestarter.herokuapp.com"  
+      }  
+      sending = m.messages.send message  
+      sending
+    end
+
+    # Sends the Game On email - if minimum has been met before deadline
+    #
+    #
+    def send_game_on(emails, event_id, event_name)
+      mandrill_to_array = []
+      # mandrill_mergevars_array =[]
+      emails.each do |email|
+        # email_url_encode = email.gsub("@", "%40")
+        this_to_object = {
+          :email => "#{email}",
+          :name => ""
+        }
+        mandrill_to_array.push(this_to_object)
+
+        # this_merge_vars = {
+        #   :rcpt => "#{email}",
+        #   :vars => [
+        #     {
+        #       "name"=> "urlemail",
+        #       "content"=> "#{email}"
+        #     }, 
+        #     {
+        #       "name"=> "replyurl",
+        #       "content"=> "http://gamestarter.herokuapp.com/reply/#{event_id}/#{email_url_encode.strip()}"
+        #     }
+        #   ]
+        # }
+        # mandrill_mergevars_array.push(this_merge_vars)
+      end
+
+      m = Mandrill::API.new
+      message = {  
+       # :merge_vars => mandrill_mergevars_array,
+       # :merge => true,
+       :subject=> "Game On: #{event_name}",  
+       :from_name=> "GameStarter <noreply@eventstarter.co>",  
+       :text=>"Your gamestarter event #{event_name} is on.  Please, plan on attending, since you are 'in'.",  
+       :to=> mandrill_to_array,  
+       :html=>"<html><h1>#{event_name} is on!</h1><p>Please, plan on attending, since you are 'in'.</p></html>",  
+       :from_email=>"sender@gamestarter.herokuapp.com"  
+      }  
+      sending = m.messages.send message  
+      sending
+    end
+
+    # Sends Game On email to ONLY new recipients after minimum has been met 
+    # -- but basically tell them its still cool to come b/c the max number has not been met
+    def send_game_already_on(email, event_id, event_name)
+      m = Mandrill::API.new
+      message = {   
+       :subject=> "Game On: #{event_name}",  
+       :from_name=> "GameStarter <noreply@eventstarter.co>",  
+       :text=>"Your GameStarter '#{event_name}' is now on. Please come out since you said your in on your RSVP.",  
+       :to=> [{
+          :email => "#{email}",
+          :name => ""
+        }],  
+       :html=>"<html><h1>Your GameStarter '#{event_name}' is now on.</h1><p>#{event_name}</p><p>The minimum number has been met for this event, but the max has not yet been met.</p></html>",  
+       :from_email=>"sender@gamestarter.herokuapp.com"  
+      }  
+      sending = m.messages.send message  
+      sending
+    end
+
+    # Sends Game off message for the following scenarios
+    #
+    # 1) The minimum number was not met by the deadline
+    # 2) The maximum number was reached by the time this user clicked the "I'm In" button
+    def send_game_rejection(email, event_id, event_name)
+      m = Mandrill::API.new
+      message = {   
+       :subject=> "Game FULL: #{event_name}",  
+       :from_name=> "GameStarter <noreply@eventstarter.co>",  
+       :text=>"Your GameStarter '#{event_name}' is full, and the maximum number of attendees has been met. We hope to catch you next time.",  
+       :to=> [{
+          :email => "#{email}",
+          :name => ""
+        }],  
+       :html=>"<html><h1>Your GameStarter '#{event_name}' is FULL.</h1><p>#{event_name}</p><p>The maximum number has been met for this event. Sorry, we hope to catch you next time.</p></html>",  
+       :from_email=>"sender@gamestarter.herokuapp.com"  
+      }  
+      sending = m.messages.send message  
+      sending
+    end
+
+    # This method sends the email out to the Creator to confirm the event is created and the invitees list has been sent to
+    # 
+    # 
+    def send_event_create_confirmation(email, event_id, event_name)
+      m = Mandrill::API.new
+      message = {   
+       :subject=> "Event Created and Sent on Gamestarter",  
+       :from_name=> "GameStarter <noreply@eventstarter.co>",  
+       :text=>"You created a new gamestarter event, and you send out emails to your list. Details attached: ...",  
+       :to=> [{
+          :email => "#{email}",
+          :name => ""
+        }],  
+       :html=>"<html><h1>GameStarter Event Created and Sent</h1><p>#{event_name}</p><p>See Event coming soon.</p></html>",  
+       :from_email=>"sender@gamestarter.herokuapp.com"  
       }  
       sending = m.messages.send message  
       sending
